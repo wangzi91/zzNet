41ad6a0eb3ed1bc4210f2f203367b164d156ebd2
zzwang <wangzizhou361@gmail.com>
2020-08-31 14:04:43 +0800

    添加确认退出

--- src/pages/layout/components/headers.vue
+++ src/pages/layout/components/headers.vue
@@ -4,13 +4,15 @@
       <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
         <el-menu-item index="1" @click="goindex">首页</el-menu-item>
         <el-menu-item index="2" @click="gonews">消息中心</el-menu-item>
-        <el-menu-item>退出</el-menu-item>
+        <el-menu-item @click="gologin">退出</el-menu-item>
       </el-menu>
     </div>
   </div>
 </template>
 
 <script>
+  import {removeToken} from '../../../utils/user'
+
   export default {
     name: 'headers',
     data () {
@@ -27,6 +29,25 @@
       },
       gonews () {
         this.$router.push('/news')
+      },
+      gologin () {
+        this.$confirm('确认退出吗？', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          removeToken()
+          this.$router.push('/login')
+          this.$message({
+            type: 'success',
+            message: '退出成功!'
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消退出'
+          })
+        })
       }
     }
   }
