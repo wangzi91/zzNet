b9ed6b3fe576de85e11267a3179a2ccb85e3177f
zzwang <wangzizhou361@gmail.com>
2020-09-03 16:20:25 +0800

    huanyuan

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const Koa = require('koa')
 const cors = require('koa2-cors')
 const app = new Koa
 const router = require('./api/text')
-// const KoaStatic = require('koa-static')
+const KoaStatic = require('koa-static')
 
-// app.use(KoaStatic(__dirname + '/dist/'))
-// router.get('/', (ctx) => {
-//   ctx.response.type = 'html'
-//   ctx.response.body = fs.createReadStream(__dirname + '/dist/index.html')
-// })
+app.use(KoaStatic(__dirname + '/dist/'))
+router.get('/', (ctx) => {
+  ctx.response.type = 'html'
+  ctx.response.body = fs.createReadStream(__dirname + '/dist/index.html')
+})
 app.use(cors())
 app.use(async (ctx, next) => {
   const start = Date.now()
