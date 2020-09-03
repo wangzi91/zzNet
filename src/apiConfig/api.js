70b9493ea5967674e17829bc653caf09a3dd64ef
zzwang <wangzizhou361@gmail.com>
2020-09-03 16:53:06 +0800

    添加env

--- src/apiConfig/api.js
+++ src/apiConfig/api.js
@@ -1,3 +1,2 @@
-// const baseUrl = 'http://127.0.0.1:3001'
-const baseUrl = 'http://119.45.218.149:3001'
+const baseUrl = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:3001' : 'http://119.45.218.149:3001'
 export default baseUrl
