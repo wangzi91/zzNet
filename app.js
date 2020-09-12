73d345d0231b80dcc23e5a9f863a10bb386de593
zzwang <wangzizhou361@gmail.com>
2020-09-12 10:53:26 +0800

    text

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const Koa = require('koa')
-const cors = require('koa2-cors')
-const app = new Koa
-const router = require('./api/text')
-const KoaStatic = require('koa-static')
-const bodyParser = require('koa-bodyparser')
 
-app.use(KoaStatic(__dirname + '/dist/'))
+const app = new Koa
+const bodyParser = require('koa-bodyparser')
+const KoaStatic = require('koa-static')
+const Router = require('koa-router')
+const router = new Router()
+const fs = require('fs')
+const user = require('./routes/user')
+
+// 加载子路由
+router.use('/index', user.routes())
 router.get('/', (ctx) => {
   ctx.response.type = 'html'
   ctx.response.body = fs.createReadStream(__dirname + '/dist/index.html')
 })
-app.use(cors())
+
+// 加载路由中间件
+app.use(router.routes())
+
+app.use(KoaStatic(__dirname + '/dist/'))
+
 app.use(bodyParser())
 app.use(async (ctx, next) => {
   const start = Date.now()
@@ -18,5 +27,5 @@ app.use(async (ctx, next) => {
   const ms = Date.now() - start
   console.log(`${ctx.method} ${ctx.url} ${ms}ms`)
 })
-app.use(router.routes())
+// app.use(router.routes())
 app.listen(3001)
