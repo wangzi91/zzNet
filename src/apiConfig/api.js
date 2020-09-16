4583230177f79667661aade2e1c1d96f32dd3795
zzwang <wangzizhou361@gmail.com>
2020-09-16 16:23:39 +0800

    3001

--- src/apiConfig/api.js
+++ src/apiConfig/api.js
@@ -1,6 +1,6 @@
 
-const ip = process.env.NODE_ENV === 'development' ? '127.0.0.1' : '119.45.218.149'
+const ip = process.env.NODE_ENV === 'development' ? '127.0.0.1:3002' : '119.45.218.149:3001'
 console.log('主机ip:', ip)
-const baseUrl = `http://${ip}:3002`
+const baseUrl = `http://${ip}`
 module.exports = baseUrl
 
