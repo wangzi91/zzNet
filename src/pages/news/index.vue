d7480a387e9ff01b267754f22c2cc08a100168a8
zzwang <wangzizhou361@gmail.com>
2021-07-23 14:07:32 +0800

    阿里云sdk 剪辑

--- src/pages/news/index.vue
+++ src/pages/news/index.vue
@@ -3,24 +3,33 @@
     <!--    <el-page-header @back="goBack" content="news">-->
     <!--    </el-page-header>-->
     <div class="index-top">this is news</div>
+    <div id="aliyun-video-editor" style="height:700px"></div> // 您可以根据需要改变container高度
+
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'index',
-    methods: {
-      goBack () {
-        console.log('go back')
-        this.$router.go(-1)
-      }
+export default {
+  name: 'index',
+  methods: {
+    goBack() {
+      console.log('go back')
+      this.$router.go(-1)
     }
+  },
+  mounted() {
+    window.AliyunVideoEditor.init({
+      container: document.getElementById('aliyun-video-editor'),
+      locale: 'zh-CN',
+      useDynamicSrc: true
+    })
   }
+}
 </script>
 
 <style scoped>
-  .index-top {
-    color: #409EFF;
-    /*margin-top: 20%;*/
-  }
+.index-top {
+  color: #409EFF;
+  /*margin-top: 20%;*/
+}
 </style>
